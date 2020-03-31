10677960379e3e5a4a3a636a7e5ac17a36e065ca
Miito <dinhlol69@gmail.com>
2020-03-31 05:52:04 -0400

    Add stable sort to steamdb web scraping

--- events/list.js
+++ events/list.js
@@ -34,7 +34,20 @@ async function list() {
       })
     }
 
-    return array
+    return array.sort((a, b) => {
+      const nameA = a.title.toUpperCase();
+      const nameB = b.title.toUpperCase();
+
+      if (nameA < nameB) {
+        return -1;
+      }
+
+      if (nameA > nameB) {
+        return 1;
+      }
+
+      return 0
+    })
   })
 
   await browser.close();
