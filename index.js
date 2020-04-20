519afa1f715d752030e68a65c35ef9f1320df858
Miito <dinhlol69@gmail.com>
2020-04-20 11:56:44 -0400

    Move from ascii-table to table dependencies

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 const cron = require('cron');
-const AsciiTable = require('ascii-table');
+const { table } = require('table');
 const format = require('date-fns/format');
 const SteamList = require('./events/SteamList');
 const EpicList = require('./events/EpicList');
@@ -13,24 +13,24 @@ require('dotenv').config();
 const freeSteamGameChannel = process.env.CHANNEL_ID;
 
 async function createTable() {
-  const SteamTable = new AsciiTable();
-  const EpicTable = new AsciiTable();
-
-  SteamTable.setHeading('Steam Games', 'Dates');
-  EpicTable.setHeading('Epic Games Store Games', 'Dates');
-
   const Steam = await SteamList();
   const Epic = await EpicList();
+  const data = [];
+
+  data.push(['Steam Games', 'Dates']);
 
   Steam.forEach((el) => {
-    SteamTable.addRow(`${el.APP_TYPE}: ${el.TITLE}`, `${format(el.START_DATE, 'MMM dd')} - ${format(el.END_DATE, 'MMM dd')}`);
+    data.push([`${el.APP_TYPE}: ${el.TITLE}`, `${format(el.START_DATE, 'MMM dd')} - ${format(el.END_DATE, 'MMM dd')}`]);
   });
+
+  data.push(['', ''])
+  data.push(['Epic Store Games', 'Dates']);
 
   Epic.forEach((el) => {
-    EpicTable.addRow(`Game: ${el.TITLE}`, `${format(el.START_DATE, 'MMM dd')} - ${format(el.END_DATE, 'MMM dd')}`);
+    data.push([`Game: ${el.TITLE}`, `${format(el.START_DATE, 'MMM dd')} - ${format(el.END_DATE, 'MMM dd')}`]);
   });
 
-  return `${SteamTable.toString()}\n${EpicTable.toString()}`;
+  return table(data);
 }
 
 client.login(process.env.DISCORD_KEY);
@@ -43,7 +43,7 @@ client.on('ready', () => {
   scheduled.addCallback(() => {
     createTable().then((result) => {
       client.channels.fetch(freeSteamGameChannel).then((reponse) => {
-        reponse.send(result, { code: 'elm' });
+        reponse.send(result, { code: 'moon' });
       });
     });
   });
